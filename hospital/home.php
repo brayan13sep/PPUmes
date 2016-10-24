78344282b3dd6a7f0e4cd512469e729f5f1dad48
checha <checha1221@gmail.com>
2016-10-24 11:01:01 -0600

    cambios en el home y nuevo formato de contacto

--- hospital/home.php
+++ hospital/home.php
@@ -37,14 +37,20 @@
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="home.php">Inicio</a></li>
+            <li role="presentation" class="active"><a href="home.php">I</a></li>
               <li class="dropdown">
+
+
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Perfil <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                 <li><a href="#">Cerrar Sesion</a></li>
                  </ul>
               </li>
           </ul>
+           <ul class="nav navbar-nav navbar-right">
+               <li><a href=""><span class="glyphicon glyphicon-user"></span> Registrar</a></li>
+
+            </ul>
         </nav>
         <h3 class="text-muted">Hospital</h3>
       </div>
