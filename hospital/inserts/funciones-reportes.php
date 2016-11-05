db0f07c4243771d092f8cb799723e2828da2e910
Brayan <brayan13sep@gmail.com>
2016-11-05 14:52:46 -0600

    agrego asignacion de madre neonato

--- hospital/inserts/funciones-reportes.php
+++ hospital/inserts/funciones-reportes.php
@@ -1,34 +1,33 @@
 <?php
- 
-require '../DataBase/db.class.php';
-  require '../DataBase/Conf.class.php';
-  $db=Db::getInstance();
-$sede        = null;
-$tipo_reporte= null;
-$fecha_inicio= null;
-$fecha_fin   = null;
-
-function imprimir($sede, $tipo_reporte, $fecha_inicio, $fecha_fin)
-{
-	if ($tipo_reporte== "No. Neonatos nacidos") {
-		echo 
-		'
-          <legend>Modulo de consultas</legend>
-          <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
-            <tr>
-              <td> Genero</td>
-              <td> RFID TAG</td>
-              <td> Peso</td>
-              <td> Sede</td>
-              <td> Fecha de nacimiento</td>
-            </tr>
-            
-          </table> ';
-		
-	}
-}
-
-
-
-
- ?>
+  require '../DataBase/db.class.php';
+    require '../DataBase/Conf.class.php';
+    $db=Db::getInstance();
+  $sede        = null;
+  $tipo_reporte= null;
+  $fecha_inicio= null;
+  $fecha_fin   = null;
+  
+  function imprimir($sede, $tipo_reporte, $fecha_inicio, $fecha_fin)
+  {
+    if ($tipo_reporte== "No. Neonatos nacidos") {
+      echo 
+      '
+            <legend>Modulo de consultas</legend>
+            <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+              <tr>
+                <td> Genero</td>
+                <td> RFID TAG</td>
+                <td> Peso</td>
+                <td> Sede</td>
+                <td> Fecha de nacimiento</td>
+              </tr>
+              
+            </table> ';
+      
+    }
+  }
+  
+  
+  
+  
+   ?>
